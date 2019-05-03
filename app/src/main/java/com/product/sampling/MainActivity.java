18b7210b45e88c91591156e1bcd448ca8c0688e2
724206429@qq.com <724206429@qq.com>
2019-05-04 00:07:37 +0800

    new project init

---
+++ app/src/main/java/com/product/sampling/MainActivity.java
@@ -0,0 +1,21 @@
+package com.product.sampling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.product.sampling.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
